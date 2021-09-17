0267b7961012c0191b71cd3908f5de6fc7fa9fe0
teradaian <teradaian@gmail.com>
2021-09-17 16:35:04 -0700

    rel vers

--- js/script.js
+++ js/script.js
@@ -2,26 +2,25 @@ const BASE_URL = 'https://api.giphy.com/v1/gifs/search?q='
 const API_KEY = 'H37gvoK5hDZU8GAWYktJwgIJRZu4zaA0'
 const $input = $('input')
 const $form = $('form')
-let numLimit = 8
-const $gifUrl = $('img') 
+const $gifUrl = $('img')
+let numLimit = 20
 
-const getRandomNumberBetween = (min,max) =>{
-    return Math.floor(Math.random()*(max-min+1)+min);
-}
+const getRandNumBetween = (min,max) => Math.floor(Math.random()*(max-min+1)+min);
 
 const handleGetData = (e) => {
     e.preventDefault();
     const query = $input.val()
     $.ajax(`${BASE_URL}${query}&limit=${numLimit}&api_key=${API_KEY}`)
         .then(data => {
-            const randomGif = data.data[getRandomNumberBetween(1, numLimit - 1)].url
+            const randomGif = data.data[getRandNumBetween(1, numLimit - 1)].images.fixed_height.url
             render(randomGif)
+            $input.val('')
         }), (err => {
             console.log(err)
         })
-  }
+}
 
-  $form.on('submit', handleGetData)
+$form.on('submit', handleGetData)
 
 function render(gif) {
     $('img').attr("src", gif)
